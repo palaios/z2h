850e6ed10e9eab0e168ed32d27e26f7723a8b8c7
Knut Andreas Kyrvestad <palaios@palaios-PC.localdomain>
2017-08-29 10:02:04 +0200

    Added javascript button to change user local storage

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,21 @@ myImage.onclick = function() {
     }
 }
 
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName(){
+    var myName = prompt('Please enter yur name.');
+    localStorage.setItem('name', myName);
+    myHeading.textCOntent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUSerName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function(){
+    setUserName();s}
